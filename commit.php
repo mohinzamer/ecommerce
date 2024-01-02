004847446dbf51662636da6b972277e1bf7f6dd0
mohin <mohin.m@technotackle.com>
2024-01-02 15:46:22 +0530

    code changes

--- commit.php
+++ commit.php
@@ -1,11 +1,13 @@
 <?php
 
 $owner = 'mohinzamer';
-$repo = 'ecommerces';
-$committer = 'mohin.m@technotackle.com';
+$repo = 'ecommerce';
+$committer = 'mohinzamer';
 $token = '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';
+$date="2024-01-02";
 
-$url = "https://api.github.com/repos/{$owner}/{$repo}/commits?committer={$committer}";
+// $url = "https://api.github.com/repos/{$owner}/{$repo}/commits?author=mohinzamer&since=".$date."T00:00:00.000+05:30&until=".$date."T23:59:59.000+05:30";
+$url = "https://api.github.com/repos/{$owner}/{$repo}/commits?author=".$committer."&since=".$date."T00:00:01Z&until=".$date."T23:59:59Z";
 
 
 $options = [
@@ -26,14 +28,18 @@ $ch = curl_init();
 curl_setopt_array($ch, $options);
 
 $response = curl_exec($ch);
-print_r($response);
+//print_r(json_decode($response, true));
 
 if (curl_errno($ch)) {
     echo 'Error: ' . curl_error($ch);
 } else {
     $decodedResponse = json_decode($response, true);
+    echo "<pre>";
     print_r($decodedResponse); // Output the response as an array
 }
+
+
+
 
 curl_close($ch);
 ?>
